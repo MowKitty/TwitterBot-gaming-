d90111e6f079766a126035519b996b22ec4efd8f
Clockknight <34818025+Clockknight@users.noreply.github.com>
2021-11-03 16:54:21 -0700

    refined goal wording

--- twitterbotgaming.py
+++ twitterbotgaming.py
@@ -1,2 +1,7 @@
 #TODO:
 #Tweet when money machine(gaming)
+#Script will tweet on a twitter account when it notices that "Money Machine" by 100 Gecs is playing on my spotify account
+
+#TODO:
+
+#TODO:
