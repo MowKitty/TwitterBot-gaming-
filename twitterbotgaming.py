03ebb021d9198152897540955ee05811b02028e7
Clockknight <34818025+Clockknight@users.noreply.github.com>
2021-11-05 11:17:48 -0700

    adjusted comments

--- twitterbotgaming.py
+++ twitterbotgaming.py
@@ -1,12 +1,15 @@
+'''
 #TODO:
-#Watch my spotify activity
+Print currently playing song
+SPOTIPY Library URL:
+    https://spotipy.readthedocs.io/en/2.19.0/?highlight=currently#spotipy.client.Spotify.currently_playing
 
 #TODO:
-#Notice when I listen to "Money Machine"
-#Include device, date, and time.
+Tweet on twitter from this script
 
 #TODO:
-#Tweet something about me listening to "Money Machine"
+Tweet something about me listening to "Money Machine"
 
 #TODO:
-#Tweet how many times I've listened to it this month
+Tweet how many times I've listened to it this month
+'''
